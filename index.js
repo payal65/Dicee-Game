a692eb2bd72fa18ea4b9ab4fbaa74c6bc8068966
payal kumari <95978126+payal65@users.noreply.github.com>
2023-08-14 12:01:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNumber1=Math.floor(Math.random() * 6);
+randomNumber1=randomNumber1+1;
+//console.log(randomNumber1);
+document.querySelector(".img1").setAttribute("src","./images/dice"+randomNumber1+".png");
+var randomNumber2=Math.floor(Math.random()*6);
+randomNumber2=randomNumber2+1;
+//console.log(randomNumber2);
+document.querySelector(".img2").setAttribute("src","./images/dice"+randomNumber2+".png");
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerText="Player 1 Wins!";
+}else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerText="Player 2 Wins!";
+}else{
+    document.querySelector("h1").innerText="Draw!";
+}
